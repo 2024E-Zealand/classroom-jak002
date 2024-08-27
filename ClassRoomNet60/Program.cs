916d75130590ab70aae9eb6ee2cd70308f719371
Jacob Sejer Kristoffersen <jak002@edu.zealand.dk>
2024-08-27 13:37:22 +0200

    Task 3 done

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ClassRoomNet60;
+
+ClassRoom dat3b = new ClassRoom();
+dat3b.StudentList = new List<Student>() { new("Jacob", 1, 30), new("Marius", 12, 5), new("Stephane", 9, 3) };
