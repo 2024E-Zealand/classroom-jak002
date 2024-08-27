2a8dc34539a9ebc146568a4d1f0dab62e6ead0a1
Jacob Sejer Kristoffersen <jak002@edu.zealand.dk>
2024-08-27 13:50:36 +0200

    Task 5 done

--- ClassRoomNet60/Student.cs
+++ ClassRoomNet60/Student.cs
@@ -18,5 +18,21 @@ namespace ClassRoomNet60
             BirthMonth = birthmonth;
             Birthday = birthday;
         }
+
+        public string Season()
+        {
+            switch (BirthMonth)
+            {
+                case 12 or 1 or 2:
+                        return "Winter";
+                case 3 or 4 or 5:
+                    return "Spring";
+                case 6 or 7 or 8:
+                    return "Summer";
+                case 9 or 10 or 11:
+                    return "Autumn";
+            }
+            return "Salt and pepper mostly";
+        }
     }
 }
