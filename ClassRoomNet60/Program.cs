113c87ea952e546ef281532eea878265d60518d3
Jacob Sejer Kristoffersen <jak002@edu.zealand.dk>
2024-08-27 13:43:09 +0200

    Task 4 done

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -2,4 +2,14 @@
 using ClassRoomNet60;
 
 ClassRoom dat3b = new ClassRoom();
+dat3b.ClassName = "3B";
+dat3b.SemesterStart = new(2024, 8, 26);
 dat3b.StudentList = new List<Student>() { new("Jacob", 1, 30), new("Marius", 12, 5), new("Stephane", 9, 3) };
+
+Console.WriteLine("Classroom name: " + dat3b.ClassName);
+Console.WriteLine("Semester start: " + dat3b.SemesterStart.ToShortDateString());
+Console.WriteLine("Students:");
+foreach(Student student in dat3b.StudentList) 
+{
+    Console.WriteLine($"\t {student.Name}, born {student.Birthday}/{student.BirthMonth}");
+}
